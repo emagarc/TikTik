9575809d0a14b36f92ca06abca02a716a0b301ee
Emanuel Garcia <emagarc@Emanuels-Air.fibertel.com.ar>
2023-04-19 17:39:32 -0300

    Sidebar almost finished

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     return (
         <div className='w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4'>
             <Link href={'/'}>
-                <div className='w-[100px] md:w-[130px] md:h-[30px]'>
+                <div className='w-[100px] md:w-[130px]'>
                     <Image
                         className='cursor-pointer'
                         src={Logo}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@ import type { AppProps } from 'next/app'
 import { useState, useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Sidebar from '../components/Sidebar';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const App = ({ Component, pageProps }: AppProps) => {
   const [isSrr, setIsSrr] = useState(true);
@@ -14,17 +15,19 @@ const App = ({ Component, pageProps }: AppProps) => {
   if (isSrr) return null;
 
   return (
-    <div>
-      <Navbar />
-      <div className='flex gap-6 md:gap-20'>
-        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
-          <Sidebar />
-        </div>
-        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
-          <Component {...pageProps} />
+    <GoogleOAuthProvider clientId="<your_client_id>">
+      <div>
+        <Navbar />
+        <div className='flex gap-6 md:gap-20'>
+          <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+            <Sidebar />
+          </div>
+          <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+            <Component {...pageProps} />
+          </div>
         </div>
       </div>
-    </div>
+    </GoogleOAuthProvider>
   );
 }
 
