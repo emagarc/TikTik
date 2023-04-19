d8c31200ad165fa2d49c8a529854918fc5783d5a
Emanuel Garcia <emagarc@Emanuels-Air.fibertel.com.ar>
2023-04-18 21:39:48 -0300

    Setup p2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,29 @@
-import '<emagarc>/styles/globals.css'
+import '../styles/globals.css';
 import type { AppProps } from 'next/app'
+import { useState, useEffect } from 'react';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const App = ({ Component, pageProps }: AppProps) => {
+  const [isSrr, setIsSrr] = useState(true);
+
+  useEffect(() => {
+    setIsSrr(false);
+  }, []);
+
+  if (isSrr) return null;
+
+  return (
+    <div>
+      Nav Bar
+      <div className='flex gap-6 md:gap-20'>
+        <div className='h-[92vh] overflow-hidden xl:hover:overflow-auto'>
+          Sidebar
+        </div>
+        <div className='mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1'>
+          <Component {...pageProps} />
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default App;
