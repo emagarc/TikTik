d2fa33299f02c1d4b04d00f2ab43b62c27fb30a9
Emanuel Garcia <emagarc@Emanuels-Air.fibertel.com.ar>
2023-04-19 03:43:03 -0300

    bug tailwind fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 module.exports = {
-  purge: [
+  content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
   ],
-  mode: 'jit',
   theme: {
     extend: {
       width: {
