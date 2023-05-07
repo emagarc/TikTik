48ac333898925f524390f567b839c1b2aa84d87f
Emanuel Garcia <wemagw@gmail.com>
2023-05-07 20:30:36 -0300

    User Store almost done

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['yt3.ggpht.com', 'avatars.githubusercontent.com']
+    domains: ['yt3.ggpht.com', 'avatars.githubusercontent.com', 'lh3.googleusercontent.com']
   }
 };
 
