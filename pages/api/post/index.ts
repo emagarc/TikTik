09ab41c1a7f896aa07cea13bc2b2dfa2daf43e26
Emanuel Garcia <wemagw@gmail.com>
2023-05-08 15:30:04 -0300

    Upload section finished

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -2,17 +2,17 @@ import { allPostsQuery } from '../../../utils/queries';
 import { client } from '../../../utils/client';
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-    name: string
-}
-
 export default async function handler(
     req: NextApiRequest,
-    res: NextApiResponse<Data>
+    res: NextApiResponse
 ) {
     if (req.method === 'GET') {
         const query = allPostsQuery();
         const data = await client.fetch(query);
         res.status(200).json(data);
+    } else if (req.method === 'POST') {
+        const document = req.body;
+        client.create(document)
+        .then(() => res.status(201).json('Video Created'))
     }
 }
