97686c8a21102490d7b2198bab141bb7d91a7ad2
Emanuel Garcia <wemagw@gmail.com>
2023-05-08 15:40:24 -0300

    Video card details fixed

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -73,7 +73,7 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
               src={post.video.asset.url}
               loop
               ref={videoRef}
-              className="lg:w[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
             ></video>
           </Link>
 
