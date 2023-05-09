70bcb91b2e20e5bbd619d8638d39c2867df30ccd
Emanuel Garcia <wemagw@gmail.com>
2023-05-09 11:50:07 -0300

    Likes section

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -23,7 +23,7 @@ const Detail = ({ postDetails }: IProps) => {
   const [isVideoMuted, setIsVideoMuted] = useState(false);
   const videoRef = useRef<HTMLVideoElement>(null);
   const router = useRouter();
-  const { userProfile } = useAuthStore();
+  const { userProfile }: any = useAuthStore();
 
   const onVideoClick = () => {
     if (playing) {
@@ -40,6 +40,18 @@ const Detail = ({ postDetails }: IProps) => {
       videoRef.current.muted = isVideoMuted;
     }
   }, [post, isVideoMuted]);
+
+  const handleLike = async (like: boolean) => {
+    if(userProfile) {
+      const { data } = await axios.put(`${BASE_URL}/api/like`, 
+      {
+        userId: userProfile._id,
+        postId: post._id,
+        like
+      })
+      setPost({ ...post, likes: data.likes });
+    }
+  }
 
   if (!post) return null;
 
@@ -117,7 +129,11 @@ const Detail = ({ postDetails }: IProps) => {
           <p className="px-10 text-lg text-gray-600">{post.caption}</p>
           <div className="mt-10 px-10">
             {userProfile && (
-              <LikeButton />
+              <LikeButton
+              likes={post.likes} 
+              handleLike={() => {handleLike(true)}}
+              handleDislike={() => {handleLike(false)}}
+              />
             )}
           </div>
           <Comments />
