d35ba01e96f02e7968a38f9d6b106c65c75e2b50
Emanuel Garcia <wemagw@gmail.com>
2023-05-15 20:38:12 -0300

    comments section done

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,17 +1,25 @@
-import create from 'zustand';
-import { persist } from 'zustand/middleware';
-import axios from 'axios';
+import create from "zustand";
+import { persist } from "zustand/middleware";
+import axios from "axios";
+
+import { BASE_URL } from "../utils/index";
 
 const authStore = (set: any) => ({
-    userProfile: null,
-    addUser: (user: any) => set({ userProfile: user}),
-    removeUser: () => set({ userProfile : null})
+  userProfile: null,
+  allUsers: [],
+  addUser: (user: any) => set({ userProfile: user }),
+  removeUser: () => set({ userProfile: null }),
+
+  fetchAllUsers: async () => {
+    const response = await axios.get(`${BASE_URL}/api/users`);
+    set({ allUsers: response.data });
+  },
 });
 
 const useAuthStore = create(
-    persist(authStore, {
-        name: 'auth'
-    })
+  persist(authStore, {
+    name: "auth",
+  })
 );
 
 export default useAuthStore;
